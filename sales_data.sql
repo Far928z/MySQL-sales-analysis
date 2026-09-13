5e16c7d0bc9a9400e86271f8f6661d2ff360e81c
Far928z <adamfuad1235@gmail.com>
2026-09-13 16:18:44 +0000

    Insert sales data into sales_data.sql

---
+++ sales_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO sales
+(Sale_ID, Sale_Date, Product, Category, Region, Quantity, Unit_Price, Total_Sales, Profit)
+VALUES
+(1, '2026-01-05', 'Laptop', 'Electronics', 'Kumasi', 2, 4500.00, 9000.00, 900.00),
+(2, '2026-01-07', 'Phone', 'Electronics', 'Accra', 5, 2500.00, 12500.00, 1250.00),
+(3, '2026-01-10', 'Rice', 'Groceries', 'Tamale', 20, 250.00, 5000.00, 600.00),
+(4, '2026-01-12', 'Headphones', 'Electronics', 'Kumasi', 10, 150.00, 1500.00, 300.00),
+(5, '2026-01-15', 'Cooking Oil', 'Groceries', 'Accra', 15, 180.00, 2700.00, 405.00),
+(6, '2026-02-03', 'Laptop', 'Electronics', 'Accra', 3, 4500.00, 13500.00, 1350.00),
+(7, '2026-02-08', 'School Bag', 'Fashion', 'Kumasi', 8, 200.00, 1600.00, 320.00),
+(8, '2026-02-11', 'Shirt', 'Fashion', 'Tamale', 12, 120.00, 1440.00, 288.00),
+(9, '2026-02-18', 'Rice', 'Groceries', 'Kumasi', 25, 250.00, 6250.00, 750.00),
+(10, '2026-02-20', 'Phone', 'Electronics', 'Tamale', 4, 2500.00, 10000.00, 1000.00),
+(11, '2026-03-02', 'Cooking Oil', 'Groceries', 'Kumasi', 18, 180.00, 3240.00, 486.00),
+(12, '2026-03-05', 'School Bag', 'Fashion', 'Accra', 10, 200.00, 2000.00, 400.00),
+(13, '2026-03-09', 'Laptop', 'Electronics', 'Tamale', 2, 4500.00, 9000.00, 900.00),
+(14, '2026-03-15', 'Shirt', 'Fashion', 'Kumasi', 15, 120.00, 1800.00, 360.00),
+(15, '2026-03-20', 'Rice', 'Groceries', 'Accra', 30, 250.00, 7500.00, 900.00);
